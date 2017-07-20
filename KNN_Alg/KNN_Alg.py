12ccd709da3bed5601baa874857e2b0cf751534c
lmd-in-learning <492556709@qq.com>
2017-07-20 21:24:08 +0800

    Add KNN algorithm

--- KNN_Alg/KNN_Alg.py
+++ KNN_Alg/KNN_Alg.py
@@ -1,1 +1,55 @@
 #KNN algorothms
+from numpy import *
+import operator
+import matplotlib
+import matplotlib.pyplot as plt
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis = 1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), 
+                                reverse = True)
+    return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLableVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLableVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLableVector
+
+if __name__ == '__main__':
+    group, labels = createDataSet()
+    result = classify0([0, 0], group, labels, 3)
+    print result
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    # print datingDataMat
+    # print datingLabels
+    # print datingLabels[0:20]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+    plt.show()
+
