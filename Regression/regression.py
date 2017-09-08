1ddf611585048b547f685bdd72496b34146ceb6f
lmd-in-learning <492556709@qq.com>
2017-09-08 23:00:34 +0800

    add regressiong alg and test data

---
+++ Regression/regression.py
@@ -0,0 +1,56 @@
+#regression
+from numpy import *
+
+def loadDataSet(fileName):
+    numFeat = len(open(fileName).readline().split('\t')) - 1
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+
+def standRegres(xArr, yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    xTx = xMat.T * xMat
+    if linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = xTx.I * (xMat.T * yMat)
+    return ws
+
+
+if __name__ == '__main__':
+    xArr, yArr = loadDataSet('ex0.txt')
+    print(xArr[0:2])
+    ws = standRegres(xArr, yArr)
+    xMat = mat(xArr)
+    yMat = mat(yArr)
+    yHat = xMat * ws
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+    xCopy = xMat.copy()
+    xCopy.sort(0)
+    yHat = xMat * ws
+    # ax.plot(xCopy[:, 1], yHat)
+    # plt.show()
+    print(corrcoef(yHat.T, yMat))
+
+
+
+
+
+
+
+
+
+
+
