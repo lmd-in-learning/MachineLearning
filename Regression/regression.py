65fba3645a812e9566ba0bd1f17ddee8e86422c5
lmd-in-learning <492556709@qq.com>
2017-09-09 17:15:31 +0800

    add stageWise alg

--- Regression/regression.py
+++ Regression/regression.py
@@ -75,9 +75,84 @@ def ridgeTest(xArr, yArr):
         wMat[i, :] = ws.T
     return wMat
 
+def stageWise(xArr, yArr, eps = 0.01, numIt = 100):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+    m, n = shape(xMat)
+    returnMat = zeros((numIt, n))
+    ws = zeros((n, 1))
+    wsTest = ws.copy()
+    wsMax = ws.copy()
+    for i in range(numIt):
+        print(ws.T)
+        lowestError = inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps * sign
+                yTest = xMat * wsTest
+                rssE = rssError(yMat.A, yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        returnMat[i, :] = ws.T
+    return returnMat
+
+def regularize(xMat):
+    inMat = xMat.copy()
+    inMeans = mean(inMat,0)
+    inVar = var(inMat,0)  
+    inMat = (inMat - inMeans)/inVar
+    return inMat
+
+# need to apply google search API
+# from time import sleep
+# import json
+# import urllib2
+# def searchForSet(retX, retY, setNum, yr, numPce, origPrc):
+#     sleep(10)
+#     myAPIstr = 'get from code.google.com'
+#     searchURL = 'https://www.googleapis.com/shopping/search/v1/public/products?key=%s&country=US&q=lego+%d&alt=json' % (myAPIstr, setNum)
+#     pg = urllib2.urlopen(searchURL)
+#     print(pg)
+#     retDict = json.loads(pg.read())
+#     for i in range(len(retDict['items'])):
+#         try:
+#             currItem = retDict['items'][i]
+#             if currItem['product']['condition'] == 'new':
+#                 newFlag = 1.0
+#             else:
+#                 newFlag = 0
+#             listOfInv = currItem['product']['inventories']
+#             for item in listOfInv:
+#                 sellingPrices = item['price']
+#                 if sellingPrices > origPrc * 0.5:
+#                     print("%d\t%d\t%d\t%f\t%f" % (yr, numPce, newFlag, origPrc, sellingPrices))
+#                     retX.append([yr, numPce, newFlag, origPrc])
+#                     retY.append(sellingPrices)
+#         except:
+#             print("problem with item %d" % i)
+
+# def setDataCollect(retX, retY):
+#     searchForSet(retX, retY, 8288, 2006, 800, 49.99)
+#     searchForSet(retX, retY, 10030, 2002, 3096, 269.99)
+#     searchForSet(retX, retY, 10197, 2007, 5195, 499.99)
+#     searchForSet(retX, retY, 10181, 2007, 3428, 199.99)
+#     searchForSet(retX, retY, 10189, 2008, 5922, 299.99)
+#     searchForSet(retX, retY, 10196, 2006, 800, 249.99)
+
 
 
 if __name__ == '__main__':
+    # lgX = []
+    # lgY = []
+    # setDataCollect(lgX, lgY)
+    # print(lgX)
+    # print(lgY)
     # xArr, yArr = loadDataSet('ex0.txt')
     # ws = standRegres(xArr, yArr)
     # xMat = mat(xArr)
@@ -101,7 +176,7 @@ if __name__ == '__main__':
     # ax.plot(xCopy[:, 1], yHat)
     # plt.show()
     # print(corrcoef(yHat.T, yMat))
-    abX, abY = loadDataSet('abalone.txt')
+    # abX, abY = loadDataSet('abalone.txt')
     # yHat01 = lwlrTest(abX[0:99], abX[0:99], abY[0:99], 0.1)
     # yHat1 = lwlrTest(abX[0:99], abX[0:99], abY[0:99], 1)
     # yHat10 = lwlrTest(abX[0:99], abX[0:99], abY[0:99], 10)
@@ -117,13 +192,16 @@ if __name__ == '__main__':
     # ws = standRegres(abX[0:99], abY[0:99])
     # yHat = mat(abX[100:199]) * ws
     # print(rssError(abY[100:199], yHat.T.A))
-    ridgeWeights = ridgeTest(abX, abY)
-    print(ridgeWeights)
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(ridgeWeights)
-    plt.show()
+    # ridgeWeights = ridgeTest(abX, abY)
+    # print(ridgeWeights)
+    # import matplotlib.pyplot as plt
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.plot(ridgeWeights)
+    # plt.show()
+    # xArr, yArr = loadDataSet('abalone.txt')
+    # print(stageWise(xArr, yArr, 0.01, 200))
+    # print(stageWise(xArr, yArr, 0.001, 5000))
 
 
 
